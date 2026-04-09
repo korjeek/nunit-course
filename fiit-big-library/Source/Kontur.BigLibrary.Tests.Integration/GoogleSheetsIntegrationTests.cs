600203130756607e84f2b79e941cfee4255045ef
korjeek <olegka823@gmail.com>
2026-04-09 22:07:13 +0500

    fix: Fix hw-5

--- fiit-big-library/Source/Kontur.BigLibrary.Tests.Integration/GoogleSheetsIntegrationTests.cs
+++ fiit-big-library/Source/Kontur.BigLibrary.Tests.Integration/GoogleSheetsIntegrationTests.cs
@@ -5,6 +5,7 @@ using FluentAssertions;
 using Google.Apis.Auth.OAuth2;
 using Google.Apis.Services;
 using Google.Apis.Sheets.v4;
+using Google.Apis.Drive.v3;
 using Google.Apis.Sheets.v4.Data;
 using NUnit.Framework;
 
@@ -15,6 +16,7 @@ public class GoogleSheetsIntegrationTests
 {
     private GoogleSheetsIntegration _integration;
     private SheetsService _sheetsService;
+    private DriveService _driveService;
     
     private const string StructureTestSpreadsheetId = "1krG2GsklEyNKlHATY2sPcTqMFqk87Nbma2sPLq7oaZ8";
 
@@ -24,12 +26,17 @@ public class GoogleSheetsIntegrationTests
         _integration = new GoogleSheetsIntegration();
 
         var credential = GoogleCredential.FromFile("Integration/secrets.json")
-            .CreateScoped(SheetsService.Scope.Spreadsheets);
+            .CreateScoped(
+                SheetsService.Scope.Spreadsheets, 
+                DriveService.Scope.DriveFile);
         
-        _sheetsService = new SheetsService(new BaseClientService.Initializer()
+        var initializer = new BaseClientService.Initializer
         {
             HttpClientInitializer = credential
-        });
+        };
+        
+        _sheetsService = new SheetsService(initializer);
+        _driveService = new DriveService(initializer);
     }
 
     [Test]
@@ -49,68 +56,35 @@ public class GoogleSheetsIntegrationTests
     [Test]
     public void TestReadEntirePage_ReturnsAllData()
     {
-        var newSpreadsheet = new Spreadsheet
-        {
+        var oldFiles = _driveService.Files.List().Execute().Files
+            .Where(f => f.Name.StartsWith("TestSheet_")); 
+        foreach(var file in oldFiles) 
+            _driveService.Files.Delete(file.Id).Execute();
+        
+        var newSpreadsheet = new Spreadsheet 
+        { 
             Properties = new SpreadsheetProperties
             {
-                Title = $"TestSheet_{Guid.NewGuid():N}"
+                Title = $"TestSheet_{Guid.NewGuid():N}" 
             }
         };
-        var createRequest = _sheetsService.Spreadsheets.Create(newSpreadsheet);
-        var createdSpreadsheet = createRequest.Execute();
+        var createdSpreadsheet = _sheetsService.Spreadsheets.Create(newSpreadsheet).Execute();
         var spreadsheetId = createdSpreadsheet.SpreadsheetId;
-        var sheetId = createdSpreadsheet.Sheets[0].Properties.SheetId;
-
-        try
-        {
-            var expectedData = new List<IList<object>>
-            {
-                new List<object> { "Название книги", "№", "Описание", "Цена, Р" },
-                new List<object> { "Книга 1", "1", "Описание 1", "100" },
-                new List<object> { "Книга 2", "2", "Описание 2", "200" }
-            };
-            
-            var valueRange = new ValueRange { Values = expectedData };
-            var updateRequest = _sheetsService.Spreadsheets.Values.Update(valueRange, spreadsheetId, "A1:D3");
-            updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
-            updateRequest.Execute();
-            
-            var actualData = _integration.ReadData(spreadsheetId, "A1:D3");
-            expectedData.Should().BeEquivalentTo(actualData);
-        }
-        finally
-        {
-            ClearSheet(spreadsheetId, sheetId!.Value);
-        }
-    }
-    
-    private void ClearSheet(string spreadsheetId, int sheetId)
-    {
-        var getRequest = _sheetsService.Spreadsheets.Get(spreadsheetId);
-        var spreadsheet = getRequest.Execute();
-        var sheet = spreadsheet.Sheets.First(s => s.Properties.SheetId == sheetId);
-        var rowCount = sheet.Properties.GridProperties.RowCount.GetValueOrDefault();
-        var columnCount = sheet.Properties.GridProperties.ColumnCount.GetValueOrDefault();
-
-        if (rowCount <= 0 || columnCount <= 0) 
-            return;
+        var spreadsheetName = createdSpreadsheet.Sheets[0].Properties.Title;
         
-        var range = $"{sheet.Properties.Title}!A1:{GetColumnLetter(columnCount)}{rowCount}";
-        
-        var clearRequest = _sheetsService.Spreadsheets.Values.Clear(new ClearValuesRequest(), spreadsheetId, range);
-        clearRequest.Execute();
-    }
-    
-    private string GetColumnLetter(int columnNumber)
-    {
-        var columnLetter = "";
-        while (columnNumber > 0)
+        var expectedData = new List<IList<object>>
         {
-            columnNumber--;
-            columnLetter = (char)('A' + columnNumber % 26) + columnLetter;
-            columnNumber /= 26;
-        }
+            new List<object> { "Название книги", "№", "Описание", "Цена, Р" },
+            new List<object> { "Книга 1", "1", "Описание 1", "100" },
+            new List<object> { "Книга 2", "2", "Описание 2", "200" }
+        };
         
-        return columnLetter;
+        var valueRange = new ValueRange { Values = expectedData };
+        var updateRequest = _sheetsService.Spreadsheets.Values.Update(valueRange, spreadsheetId, spreadsheetName);
+        updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.UpdateRequest.ValueInputOptionEnum.RAW;
+        updateRequest.Execute();
+        
+        var actualData = _integration.ReadData(spreadsheetId, spreadsheetName);
+        expectedData.Should().BeEquivalentTo(actualData);
     }
 }
