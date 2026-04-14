5dba52932ddd8abf7a8c89a12ff3ff68ea2f6f54
korjeek <olegka823@gmail.com>
2026-04-14 12:01:24 +0500

    fix: Fix hw-6
    Signed-off-by: korjeek <olegka823@gmail.com>

--- fiit-big-library/Source/Kontur.BigLibrary.Tests.ApiSystem/BooksApiTests.cs
+++ fiit-big-library/Source/Kontur.BigLibrary.Tests.ApiSystem/BooksApiTests.cs
@@ -47,6 +47,9 @@ public class BooksApiTests : BooksApiTestBase
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
         AsserResponseByMessage(response, "Книга свободна.");
+        
+        var queue = GetReadersInQueue(bookId, token);
+        queue.Should().BeEmpty();
     }
     
     [Test]
@@ -65,6 +68,9 @@ public class BooksApiTests : BooksApiTestBase
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
         AsserResponseByMessage(response, "Вы встали в очередь.");
+        
+        var queue = GetReadersInQueue(bookId, token2);
+        queue.Should().Contain(r => r.UserName == email2);
     }
     
     [Test]
@@ -84,6 +90,9 @@ public class BooksApiTests : BooksApiTestBase
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
         AsserResponseByMessage(response, "Вы уже стоите в очереди.");
+        
+        var queue = GetReadersInQueue(bookId, token2);
+        queue!.Count(r => r.UserName == email2).Should().Be(1);
     }
 
     [Test]
@@ -100,6 +109,9 @@ public class BooksApiTests : BooksApiTestBase
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
         AsserResponseByMessage(response, "Вы уже взяли эту книгу.");
+        
+        var queue = GetReadersInQueue(bookId, token);
+        queue.Should().BeEmpty();
     }
     
     [Test]
@@ -115,6 +127,9 @@ public class BooksApiTests : BooksApiTestBase
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
         AsserResponseByMessage(response, "Вы взяли книгу.");
+        
+        var readers = GetBookReaders(bookId, token);
+        readers.Should().Contain(r => r.UserName == email);
     }
 
     [Test]
@@ -133,6 +148,9 @@ public class BooksApiTests : BooksApiTestBase
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
         AsserResponseByMessage(response, "Книга занята.");
+        
+        var readers = GetBookReaders(bookId, token2);
+        readers.Should().NotContain(r => r.UserName == email2);
     }
 
     [Test]
@@ -141,16 +159,26 @@ public class BooksApiTests : BooksApiTestBase
         // Arrange
         var (email1, _, token1) = CreateUser();
         var (email2, _, token2) = CreateUser();
+        var (email3, _, token3) = CreateUser();
         var bookId = CreateBook(token1).Id.ToString()!;
         
-        booksApiClient.EnqueueBook(bookId, email1, token1);
-        
+        booksApiClient.CheckoutBook(bookId, email1, token1);
+        booksApiClient.EnqueueBook(bookId, email2, token2);
+        booksApiClient.ReturnBook(bookId, email1, token1);
         // Act
-        var response = booksApiClient.CheckoutBook(bookId, email2, token2);
+        var response = booksApiClient.CheckoutBook(bookId, email3, token3);
+        var response2 = booksApiClient.CheckoutBook(bookId, email2, token2);
 
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
-        AsserResponseByMessage(response, "Вы взяли книгу.");
+        AsserResponseByMessage(response, "Не ваша очередь.");
+        var readers = GetBookReaders(bookId, token1);
+        readers.Should().NotContain(r => r.UserName == email3);
+        
+        response2.StatusCode.Should().Be(HttpStatusCode.OK);
+        AsserResponseByMessage(response2, "Вы взяли книгу.");
+        readers = GetBookReaders(bookId, token2);
+        readers.Should().Contain(r => r.UserName == email2);
     }
 
     [Test]
@@ -171,6 +199,9 @@ public class BooksApiTests : BooksApiTestBase
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.OK);
         AsserResponseByMessage(response, "Вы взяли книгу.");
+        
+        var readers = GetBookReaders(bookId, token2);
+        readers.Should().Contain(r => r.UserName == email2);
     }
 
     private static void AsserResponseByMessage(RestResponse response, string msg)
