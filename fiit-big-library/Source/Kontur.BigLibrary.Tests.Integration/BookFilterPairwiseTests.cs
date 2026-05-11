be651fa79eae1f11f0ff12c7905b4d275618113a
korjeek <olegka823@gmail.com>
2026-05-12 02:23:30 +0500

    feat: Hw-10 Solved

---
+++ fiit-big-library/Source/Kontur.BigLibrary.Tests.Integration/BookFilterPairwiseTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Kontur.BigLibrary.Service.Contracts;
+using NUnit.Framework;
+
+namespace Kontur.BigLibrary.Tests.Integration;
+
+[TestFixture]
+public class BookFilterPairwiseTests
+{
+    private static IEnumerable<TestCaseData> GetPairwiseFilterData()
+    {
+        yield return new TestCaseData(new BookFilter
+        {
+            Query = "C#",
+            Order = BookOrder.ByRankAndLastAdding,
+            IsBusy = true,
+            RubricSynonym = "programming",
+            Limit = 10,
+            Offset = 0
+        }).SetName("TC1_FullFilter_WithRankSorting");
+        
+        yield return new TestCaseData(new BookFilter
+        {
+            Query = null,
+            Order = BookOrder.ByLastAdding,
+            IsBusy = null,
+            RubricSynonym = null,
+            Limit = null,
+            Offset = null
+        }).SetName("TC2_EmptyFilter_DefaultBehavior");
+        
+        yield return new TestCaseData(new BookFilter
+        {
+            Query = "",
+            Order = BookOrder.ByRankAndLastAdding,
+            IsBusy = null,
+            RubricSynonym = "programming",
+            Limit = null,
+            Offset = 10
+        }).SetName("TC3_RankSorting_WithoutQuery_ShouldFallback");
+        
+        yield return new TestCaseData(new BookFilter
+        {
+            Query = "Clean Code",
+            Order = BookOrder.ByLastAdding,
+            IsBusy = false,
+            RubricSynonym = null,
+            Limit = 50,
+            Offset = null
+        }).SetName("TC4_QueryOnly_WithDefaultSorting");
+        
+        yield return new TestCaseData(null)
+            .SetName("TC5_NullFilterObject_ShouldThrowOrHandle");
+    }
+
+    [TestCaseSource(nameof(GetPairwiseFilterData))]
+    public async Task SelectBooksSummary_PairwiseTests(BookFilter filter)
+    {
+        await Task.CompletedTask;
+    }
+}
---
+++ fiit-big-library/Source/Kontur.BigLibrary.Tests.UI.PW/PlaywrightTests/BookCreatureValidTests.cs
@@ -0,0 +1,90 @@
+﻿using Kontur.BigLibrary.Tests.UI.PW.PageObjects.Controls;
+using Kontur.BigLibrary.Tests.UI.PW.PageObjects.Pages;
+using Kontur.BigLibrary.Tests.UI.PW.PlaywrightCore;
+
+namespace Kontur.BigLibrary.Tests.UI.PW.PlaywrightTests;
+
+[NonParallelizable]
+[WithAuth("test@mail.com", "Test123456!")]
+public class BookCreatureValidTests: TestBase
+{
+    private AddBookModal _bookModal;
+    private string? _tempFilePath;
+    
+    [SetUp]
+    public async Task SetUp()
+    {
+        var mainPage = await Navigation.GoToPageAsync<MainPage>();
+        _bookModal = await mainPage.AddBookButton.ClickAndOpenModalAsync<AddBookModal>();
+    }
+    
+    [TearDown]
+    public void Cleanup()
+    {
+        if (File.Exists(_tempFilePath)) 
+            File.Delete(_tempFilePath);
+    }
+    
+    [TestCase("", false)]
+    [TestCase("a", true)]
+    [TestCase("abcdefghijklmnopqrst", true)]
+    [TestCase("abcdefghijklmnopqrstu", false)]
+    [TestCase("2147483647", true)]
+    [TestCase("2147483648", false)]
+    [TestCase("-9999999999999999999", true)]
+    [TestCase("@#$%^&*()_+-=", true)]
+    public async Task NameAuthorDescFields_ValidationTests(string name, bool isValid)
+    {
+        await _bookModal.NameInput.FillAsync(name);
+        await _bookModal.AddBookSubmit.ClickAsync();
+        var nameResult = await _bookModal.NameInput.IsValid();
+        
+        await _bookModal.AuthorInput.FillAsync(name);
+        await _bookModal.AddBookSubmit.ClickAsync();
+        var authorResult = await _bookModal.AuthorInput.IsValid();
+        
+        await _bookModal.DescriptionInput.FillAsync(name);
+        await _bookModal.AddBookSubmit.ClickAsync();
+        var descriptionResult = await _bookModal.DescriptionInput.IsValid();
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(nameResult, Is.EqualTo(isValid));
+            Assert.That(authorResult, Is.EqualTo(isValid));
+            Assert.That(descriptionResult, Is.EqualTo(isValid));
+        });
+    }
+
+    [TestCase(153600, ".jpg", true)]
+    [TestCase(153601, ".jpg", false)]
+    [TestCase(1000, ".png", false)]
+    public async Task ImageUpload_ValidationTests(int sizeInBytes, string format, bool isValid)
+    {
+        _tempFilePath = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}{format}");
+        await File.WriteAllBytesAsync(_tempFilePath, new byte[sizeInBytes]);
+
+        await _bookModal.UploadImage.SetInputFilesAsync(_tempFilePath);
+        var result = await _bookModal.UploadImage.IsValid();
+        await _bookModal.AddBookSubmit.ClickAsync();
+        
+        Assert.That(isValid, Is.EqualTo(result));
+    }
+
+    [TestCase("", false)]
+    [TestCase("0", true)]
+    [TestCase("0,0", false)]
+    [TestCase("0.0", false)]
+    [TestCase("-1", false)]
+    [TestCase("2147483647", true)]
+    [TestCase("2147483648", false)]
+    [TestCase("abc", false)]
+    [TestCase("@#$%^&*()_+-=", false)]
+    public async Task PriceFields_ValidationTests(string price, bool isValid)
+    {
+        await _bookModal.PriceInput.FillAsync(price);
+        await _bookModal.AddBookSubmit.ClickAsync();
+        var result = await _bookModal.PriceInput.IsValid();
+        
+        Assert.That(isValid, Is.EqualTo(result));
+    }
+}
