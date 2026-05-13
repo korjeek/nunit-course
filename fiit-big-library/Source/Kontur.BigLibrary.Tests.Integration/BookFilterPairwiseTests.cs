d8118313458231eb0a1fb093d8e8a24d813050c1
korjeek <olegka823@gmail.com>
2026-05-13 11:33:53 +0500

    fix: Fix Hw-10
    Signed-off-by: korjeek <olegka823@gmail.com>

--- fiit-big-library/Source/Kontur.BigLibrary.Tests.Integration/BookFilterPairwiseTests.cs
+++ fiit-big-library/Source/Kontur.BigLibrary.Tests.Integration/BookFilterPairwiseTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Kontur.BigLibrary.Service.Contracts;
 using NUnit.Framework;
 
@@ -8,55 +7,27 @@ namespace Kontur.BigLibrary.Tests.Integration;
 [TestFixture]
 public class BookFilterPairwiseTests
 {
-    private static IEnumerable<TestCaseData> GetPairwiseFilterData()
+    [Test, Pairwise]
+    public async Task SelectBooksSummary_Pairwise(
+        [Values("c#", "", null)] string query,
+        [Values(BookOrder.ByRankAndLastAdding, BookOrder.ByLastAdding)] BookOrder order,
+        [Values(true, false, null)] bool? isBusy,
+        [Values("programming", null)] string rubricSynonym,
+        [Values("csharp", null)] string synonym,
+        [Values(10, null)] int? limit,
+        [Values(0, null)] int? offset)
     {
-        yield return new TestCaseData(new BookFilter
+        var filter = new BookFilter
         {
-            Query = "C#",
-            Order = BookOrder.ByRankAndLastAdding,
-            IsBusy = true,
-            RubricSynonym = "programming",
-            Limit = 10,
-            Offset = 0
-        }).SetName("TC1_FullFilter_WithRankSorting");
+            Query = query,
+            Order = order,
+            IsBusy = isBusy,
+            RubricSynonym = rubricSynonym,
+            Synonym = synonym,
+            Limit = limit,
+            Offset = offset
+        };
         
-        yield return new TestCaseData(new BookFilter
-        {
-            Query = null,
-            Order = BookOrder.ByLastAdding,
-            IsBusy = null,
-            RubricSynonym = null,
-            Limit = null,
-            Offset = null
-        }).SetName("TC2_EmptyFilter_DefaultBehavior");
-        
-        yield return new TestCaseData(new BookFilter
-        {
-            Query = "",
-            Order = BookOrder.ByRankAndLastAdding,
-            IsBusy = null,
-            RubricSynonym = "programming",
-            Limit = null,
-            Offset = 10
-        }).SetName("TC3_RankSorting_WithoutQuery_ShouldFallback");
-        
-        yield return new TestCaseData(new BookFilter
-        {
-            Query = "Clean Code",
-            Order = BookOrder.ByLastAdding,
-            IsBusy = false,
-            RubricSynonym = null,
-            Limit = 50,
-            Offset = null
-        }).SetName("TC4_QueryOnly_WithDefaultSorting");
-        
-        yield return new TestCaseData(null)
-            .SetName("TC5_NullFilterObject_ShouldThrowOrHandle");
-    }
-
-    [TestCaseSource(nameof(GetPairwiseFilterData))]
-    public async Task SelectBooksSummary_PairwiseTests(BookFilter filter)
-    {
         await Task.CompletedTask;
     }
 }
